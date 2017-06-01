6ebf56188424085557e4bf8ca6ca7bf2e864874c
jakebillings <jake@jakebillings.com>
2017-05-31 18:18:11 -0600

    Comment out print statements in labouchere so we can run 10,000,000 iterations for studies

--- labouchere.py
+++ labouchere.py
@@ -15,14 +15,12 @@ def flip_coin():
 #
 # See: https://en.wikipedia.org/wiki/Labouch%C3%A8re_system
 def gamble(sequence, balance):
-    # If a random boolean is true, we won.
-    won = flip_coin()
-
     # If the sequence is empty, the labouchere system says that the round is over.
     # End the recursion. This is essentially a win.
     if len(sequence) < 1:
-        print "Aborting because the sequence is empty"
-        return balance
+        # print "Aborting because the sequence is empty"
+        # Return 0 and the initial balance because no bet was made
+        return 0, balance
 
     # If the sequence is of length 1, the bet is the number in the sequence. Otherwise, it is the first number
     # added to the last number.
@@ -34,17 +32,25 @@ def gamble(sequence, balance):
     # You can't bet more money than you have (this isn't Wall Street), so
     # betting more than the initial balance ends recursion. This is essentially a loss.
     if bet > balance:
-        print "Aborting because you're broke", balance
-        return balance
+        # print "Aborting because you're broke", balance
+        # Return 0 and the initial balance because no bet was made
+        return 0, balance
 
-    print "Betting " + str(bet) + " with a balance of " + str(balance) + " due to sequence " + str(sequence)
+    # If a random boolean is true, we won.
+    won = flip_coin()
+
+    # print "Betting " + str(bet) + " with a balance of " + str(balance) + " due to sequence " + str(sequence)
 
     # Add or subtract from the balance based on the result of the bet and then play the next round.
     # Labouchere states that the first and last numbers of the sequence are removed in the event of a win, and the
     # amount of the bet is added to the end of the sequence in the event of a loss.
     if won:
-        print "won"
-        return gamble(sequence[1:-1], balance+bet)
+        # print "won"
+        bets, resulting_balance = gamble(sequence[1:-1], balance+bet)
+        # Increment the number of bets because we made a one and return the resulting balance
+        return bets+1, resulting_balance
     else:
-        print "lost"
-        return gamble(sequence+[bet], balance-bet)
+        # print "lost"
+        bets, resulting_balance = gamble(sequence+[bet], balance-bet)
+        # Increment the number of bets because we made a one and return the resulting balance
+        return bets+1, resulting_balance
