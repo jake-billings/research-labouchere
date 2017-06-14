45b0dc2e70db01b8c22bc51cdced7946d2804624
jakebillings <jake@jakebillings.com>
2017-06-14 12:16:21 -0600

    Update spacing in export_csv.py

--- export_csv.py
+++ export_csv.py
@@ -1,12 +1,15 @@
 # Copyright (c) 2017 [Jake Billings]
 # See LICENSE for more information
 
+
 # Exports each key value pair as a row in a csv file
 def export_dict_as_csv(d, name='export.csv'):
     with open(name, 'w') as wfile:
         for key in d:
             wfile.write(str(key)+','+str(d[key])+'\n')
 
+
+# Exports a 2D array as a csv file
 def export_array_as_csv(d, name='export.csv'):
     with open(name, 'w') as wfile:
         for row in d:
